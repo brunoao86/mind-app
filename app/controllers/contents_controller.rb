f51726094524f16855241dbb63c318d01a3feaa9
Bruno Oliveira <brunoao86@gmail.com>
2020-11-28 17:41:42 -0300

    Adiciona action para salvar conteúdo

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -8,4 +8,20 @@ class ContentsController < ApplicationController
   def new
     @content = Content.new
   end
+
+  def create
+    @content = current_user.contents.build(content_params)
+
+    if @content.save
+      redirect_to contents_path, notice: 'Content successfully created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def content_params
+    params.require(:content).permit(:title, :description)
+  end
 end
