f7dcef8ee74ba647600bb271d34088588b9c38b7
Bruno Oliveira <brunoao86@gmail.com>
2020-11-28 18:48:27 -0300

    Adiciona deleção de Conteúdo
    Tarefa: https://trello.com/c/L5akdOFs/19-criar-o-crud-de-conte%C3%BAdo

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,13 +1,12 @@
 class ContentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_content, only: [:show, :edit, :update, :destroy]
 
   def index
     @contents = current_user.contents
   end
 
-  def show
-    @content = Content.find(params[:id])
-  end
+  def show; end
 
   def new
     @content = Content.new
@@ -23,13 +22,9 @@ class ContentsController < ApplicationController
     end
   end
 
-  def edit
-    @content = Content.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @content = Content.find(params[:id])
-
     if @content.update(content_params)
       redirect_to contents_path, notice: 'Content successfully updated!'
     else
@@ -37,7 +32,17 @@ class ContentsController < ApplicationController
     end
   end
 
+  def destroy
+    @content.destroy
+
+    redirect_to contents_path, notice: 'Content successfully destroyed!'
+  end
+
   private
+
+  def set_content
+    @content = Content.find(params[:id])
+  end
 
   def content_params
     params.require(:content).permit(:title, :description)
