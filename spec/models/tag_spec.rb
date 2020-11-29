226be4348b30134e58ca9af5ae374d833ccd1576
Bruno Oliveira <brunoao86@gmail.com>
2020-11-29 19:15:47 -0300

    Adiciona teste para modelo Tag e ajusta teste HomeController

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(email: 'teste@teste.com', password: '123456') }
+  subject { Tag.new(name: 'TestTag', user: user) }
+
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should have_many(:tag_contents).dependent(:destroy) }
+    it { should have_many(:contents).through(:tag_contents) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name).scoped_to(:user_id).case_insensitive }
+  end
 end
--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
 
-  describe "GET /index" do
+  describe "GET /home" do
     it "returns http success" do
-      get "/home/index"
-      expect(response).to have_http_status(:success)
+      get "/home"
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to(new_user_session_path)
     end
   end
 
