9aecff494506a41b5c6fd7463783219af16e4a0b
Bruno Oliveira <brunoao86@gmail.com>
2020-11-29 16:23:29 -0300

    Exibe tags do usuário na página principal
    Tarefa: https://trello.com/c/ODvkwZKD/23-exibir-as-tags-do-usu%C3%A1rio-na-tela-inicial

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @tags_data = current_user.contents.joins(:tags).group('tags.name').count
   end
 end
